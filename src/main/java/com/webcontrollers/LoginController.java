46105afa387f1593e51f1adea3018d377bfc40ce
Muhammad Zain Ul Haq <mzuhaq7@gmail.com>
2021-01-27 17:06:17 +0500

    Initial Commit

---
+++ src/main/java/com/webcontrollers/LoginController.java
@@ -0,0 +1,69 @@
+package com.webcontrollers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.services.loginservices;
+
+
+
+@Controller
+@RequestMapping("/loginform")
+public class LoginController {
+	@Autowired
+	loginservices login_services;
+	
+	@GetMapping
+	public ModelAndView check(){
+		ModelAndView mv=new ModelAndView();
+		mv.setViewName("login");
+		return mv;
+	}
+	
+	@PostMapping
+	public ModelAndView logincontroller(@RequestParam("username") String userName,@RequestParam("password") String password,HttpSession session) {
+		
+		 SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+		    Date date = new Date();
+		        
+		String user_status=login_services.login(userName, password);
+		
+		session.setAttribute("time", date.getMinutes());
+		if(user_status=="customer") {
+			//add customer in CustomerList in restaurant class if already not present
+			//setting session attributes for global use
+			session.setAttribute("username", userName);
+			session.setAttribute("permission", "customer");
+		}
+		else if(user_status=="salesman") {
+			session.setAttribute("username", userName);
+			session.setAttribute("permission", "salesman");
+		}
+		else if(user_status=="owner") {
+			session.setAttribute("username", userName);
+			session.setAttribute("permission", "owner");
+			
+			
+			
+		}
+		else {
+			ModelAndView mv=new ModelAndView();
+			mv.setViewName("login");
+			mv.addObject("msg","error");
+			return mv;
+			}
+		return new ModelAndView("redirect: /FoodFamily/home");
+	}
+
+	
+}
